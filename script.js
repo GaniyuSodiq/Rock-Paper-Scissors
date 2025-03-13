44b87359e59a4d9bd39adc769cdd380619ea306e
Sodiq Olaniyi <82044466+GaniyuSodiq@users.noreply.github.com>
2025-03-13 06:52:40 +0100

    Experiment to understand Math.random() & .floor()
    Math.random produces numbers from 0 to 0.999999.. etc
    Math.floor removes the decimal part
    so Math.floor(Math.random()); will always be 0
    solution is to use the decimal part before Math.floor
    so Math.random() * 3; will 0 to 0.299999;
    Math.floor(Math.random() * 3); will be 0, 1, or 2
    and we cn get 1, 2, or 3 by Math.floor(Math.random() * 3) + 1;

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello Rock!")
+function getComputerChoice(){
+
+    let randomNumb = Math.random() * 3;
+    let computerChoice = Math.floor(randomNumb)
+    console.log("random number is " + randomNumb);
+    console.log("random number floored is " + computerChoice);
+
+    // let computerChoice = Math.floor(Math.random() * 3) + 1;
+    // if (computerChoice === 1){
+    //     return console.log("Rock");
+    // } else if (computerChoice === 2){
+    //     return console.log("Paper");
+    // } else {
+    //     return console.log("Scissors");
+    // }
+}
+
+getComputerChoice();
