04d9289ddd9921e80eca5741a53ae3cedbf1079c
Sodiq Olaniyi <82044466+GaniyuSodiq@users.noreply.github.com>
2025-03-13 08:55:58 +0100

    Add logic for the game play
    This was a challenge but i did it my own way
    it works
    i will see other people's way too and learn from them

--- script.js
+++ script.js
@@ -16,28 +16,73 @@ function getComputerChoice(){
 
     let computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1){
-        return computerChoice = "Rock";
+        return computerChoice = "rock";
     } else if (computerChoice === 2){
-        return computerChoice = "Paper";
+        return computerChoice = "paper";
     } else {
-        return computerChoice = "Scissors";
+        return computerChoice = "scissors";
     }
 }
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 // Write the logic to get the human choice
 
 
 
 function getHumanChoice() {
-    let humanChoice = prompt("", "Rock, Paper, or Scissors");
-    return console.log(humanChoice);
+    let humanChoice = prompt("", "rock, paper, or scissors").toLowerCase();
+    return humanChoice;
 }
-getHumanChoice();
+//console.log(getHumanChoice());
 
 
 // Write the logic to play a single round
 
 function playRound(humanChoice, computerChoice){
-
+    if (humanChoice === "rock" && computerChoice === "paper"){
+        computerScore++;
+        return ( 
+            "You lose!, Paper beats Rock",
+            console.log("Computer Played: " + computerChoice)
+        );        
+    } 
+    if (humanChoice === "paper" && computerChoice === "rock"){
+        humanScore++;
+        return ("You win!, Paper beats Rock", 
+        console.log("Computer Played: " + computerChoice)  );     
+    } 
+    if (humanChoice === "rock" && computerChoice === "rock"){
+        return("It is a draw", 
+        console.log("Computer Played: " + computerChoice) );     
+    }
+    if (humanChoice === "paper" && computerChoice === "paper"){
+        return ("It is a draw", 
+        console.log("Computer Played: " + computerChoice)  );     
+    }
+    if (humanChoice === "scissors" && computerChoice === "scissors"){
+        return ("It is a draw" , 
+        console.log("Computer Played: " + computerChoice)  );    
+    }     
+    if (humanChoice === "scissors" && computerChoice === "rock"){
+        computerScore++;
+        return ("You Lose, Rock crushes Scisors", 
+        console.log("Computer Played: " + computerChoice));     
+    } 
+    if (humanChoice === "rock" && computerChoice === "scissors"){
+        humanScore++;
+        return ("You win!, Rock crushes Scisors", 
+        console.log("Computer Played: " + computerChoice) );   
+    }
+    if (humanChoice === "scissors" && computerChoice === "paper"){
+        humanScore++;
+        return ("You win!, Scissors tears Paper", 
+        console.log("Computer Played: " + computerChoice));
+    }
+    if (humanChoice === "paper" && computerChoice === "scissors"){
+        computerScore++;
+        return ("You lose!, Scissors tears Paper", 
+        console.log("Computer Played: " + computerChoice));
+    }
 }
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
