f74a9198e4561e5fca060a8d0c841dd4891afd0d
Sodiq Olaniyi <82044466+GaniyuSodiq@users.noreply.github.com>
2025-03-14 07:09:37 +0100

    Remove old codes
    there are codes that i used to understand Math.random
    and the Math.floor.
    I commented them out beore now but i just read that it is not a
    good practice to keep old code in the code file.
    Let git do its work - Check git if you need your old code.

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 
 // Write the logic to get the computer choice
-function getComputerChoice(){
-    // let randomNumb = Math.random() * 3;
-    // let computerChoice = Math.floor(randomNumb)
-    // console.log("random number is " + randomNumb);
-    // console.log("random number floored is " + computerChoice);
+function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-    if (computerChoice === 1){
+    if (computerChoice === 1) {
         return computerChoice = "rock";
-    } else if (computerChoice === 2){
+    } else if (computerChoice === 2) {
         return computerChoice = "paper";
     } else {
         return computerChoice = "scissors";
@@ -21,8 +17,6 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
 
 // Declare the players score 
 let humanScore = 0;
@@ -30,63 +24,63 @@ let computerScore = 0;
 
 
 // Write the logic to play the entire game
-function playGame(){
-    
-     // Write the logic to play a single round
-    function playRound(humanChoice, computerChoice){ 
-        if (humanChoice === "rock" && computerChoice === "paper"){
+function playGame() {
+
+    // Write the logic to play a single round
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "paper") {
             computerScore++;
             return console.log("You lose! Computer Played: " + computerChoice + ". Paper wraps Rock. ");
-        } 
-        if (humanChoice === "paper" && computerChoice === "rock"){
+        }
+        if (humanChoice === "paper" && computerChoice === "rock") {
             humanScore++;
             return console.log("You win!, Paper wraps Rock. Computer Played: " + computerChoice);
-        } 
-        if (humanChoice === "rock" && computerChoice === "rock"){
-            return console.log("It is a draw. Computer Played: " + computerChoice); 
         }
-        if (humanChoice === "paper" && computerChoice === "paper"){
-            return console.log("It is a draw. Computer Played: " + computerChoice); 
+        if (humanChoice === "rock" && computerChoice === "rock") {
+            return console.log("It is a draw. Computer Played: " + computerChoice);
         }
-        if (humanChoice === "scissors" && computerChoice === "scissors"){
-            return console.log("It is a draw. Computer Played: " + computerChoice);   
-        }     
-        if (humanChoice === "scissors" && computerChoice === "rock"){
+        if (humanChoice === "paper" && computerChoice === "paper") {
+            return console.log("It is a draw. Computer Played: " + computerChoice);
+        }
+        if (humanChoice === "scissors" && computerChoice === "scissors") {
+            return console.log("It is a draw. Computer Played: " + computerChoice);
+        }
+        if (humanChoice === "scissors" && computerChoice === "rock") {
             computerScore++;
-            return console.log("You Lose, Rock crushes Scisors. Computer Played: " + computerChoice);     
-        } 
-        if (humanChoice === "rock" && computerChoice === "scissors"){
+            return console.log("You Lose, Rock crushes Scisors. Computer Played: " + computerChoice);
+        }
+        if (humanChoice === "rock" && computerChoice === "scissors") {
             humanScore++;
             return console.log("You win!, Rock crushes Scisors. Computer Played: " + computerChoice);
         }
-        if (humanChoice === "scissors" && computerChoice === "paper"){
+        if (humanChoice === "scissors" && computerChoice === "paper") {
             humanScore++;
             return console.log("You win!, Scissors tears Paper. Computer Played: " + computerChoice);
         }
-        if (humanChoice === "paper" && computerChoice === "scissors"){
+        if (humanChoice === "paper" && computerChoice === "scissors") {
             computerScore++;
             return console.log("You lose!, Scissors tears Paper. Computer Played: " + computerChoice);
         }
     }
 
     return playRound(getHumanChoice(), getComputerChoice());
-    
+
 }
 
 
-for (let i = 1; i <= 5; i++){
+for (let i = 1; i <= 5; i++) {
     playGame();
 }
 
-if (humanScore > computerScore){
+if (humanScore > computerScore) {
     console.log("##### AFTER 5 ROUNDS #####")
     console.log("YOU WON!");
     console.log("Your score is: " + humanScore + ", Computer score is: " + computerScore);
-} else if (humanScore < computerScore){
+} else if (humanScore < computerScore) {
     console.log("##### AFTER 5 ROUNDS #####")
     console.log("YOU LOST!");
     console.log("Your score is: " + humanScore + ", Computer score is: " + computerScore);
-} else{
+} else {
     console.log("##### AFTER 5 ROUNDS #####")
     console.log("IT'S DRAW");
     console.log("Your score is: " + humanScore + ", Computer score is: " + computerScore);
